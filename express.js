eba07b9c41064a7c13786483c06ae53d3b6f9a63
Jurek <jerzykomorowski77@gmail.com>
2023-10-06 13:59:22 +0200

    test

---
+++ express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+
+app.get('/', (req, res) => {
+    res.send({ message: "Działamy" });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+});
