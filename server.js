45f79a660cb6f449bd41c71585d2ac9358f1e5f3
Jurek <jerzykomorowski77@gmail.com>
2023-10-07 10:13:26 +0200

    make it better

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-    res.send({ message: "Działamy" });
+    res.send({ message: "Działamy na server" });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT} `);
 });
