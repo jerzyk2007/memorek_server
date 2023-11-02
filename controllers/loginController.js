fccd1f7fe0df441cee214038315af87cb30b9d26
Jurek <jerzykomorowski77@gmail.com>
2023-11-02 19:40:14 +0100

    .

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -32,7 +32,10 @@ const handleLogin = async (req, res) => {
         foundUser.refreshToken = refreshToken;
         const result = await foundUser.save();
 
-        res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'none', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+        // res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'none', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+
+        res.cookie('access_token', refreshToken, { domain: "localhost", path: '/', expires: new Date(Date.now() + 9000000), httpOnly: false });
+
         // res.cookie('jwt', refreshToken,
         //     {
         //         sameSite: "None",
