83e4177eee0d640f39982a5fa2718f0440d728ae
Jurek <jerzykomorowski77@gmail.com>
2023-11-02 13:57:20 +0100

    removed /

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -5,7 +5,7 @@ const allowedOrigins = [
     "http://localhost:3000",
     "http://127.0.0.1:3500",
     "http://localhost:3500",
-    "https://front-web.pl/",
+    "https://front-web.pl",
     "https://front-web.pl/memorek"
 
 
