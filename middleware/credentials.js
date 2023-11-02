45c93d2e528c38b46ec925e90a4b43a57420c8b3
Jurek <jerzykomorowski77@gmail.com>
2023-11-02 13:37:39 +0100

    add res.header to credential.js

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -3,7 +3,13 @@ const allowedOrigins = require('../config/allowedOrigins');
 const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
         res.header('Access-Control-Allow-Credentials', true);
+    }
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        return res.status(200).json({});
     }
     next();
 };
