eabffd72937f38e914f90bb780ae0a2f0e9931fa
Jurek <jerzykomorowski77@gmail.com>
2023-11-02 14:20:44 +0100

    res.header('Access-Control-Allow-Origin', origin);

---
+++ controllers/loginController.js
@@ -0,0 +1,59 @@
+const User = require('../model/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+// const handleLogin = async (req, res) => {
+//     const { user, pwd } = req.body;
+//     if (!user || !pwd) {
+//         return res.status(400).json({ 'message': 'Username and password are required.' });
+//     }
+//     const foundUser = await User.findOne({ username: user }).exec();
+//     if (!foundUser) {
+//         return res.sendStatus(401); // unauthorized
+//     }
+//     // evaluate password
+//     const match = await bcrypt.compare(pwd, foundUser.password);
+//     if (match) {
+//         const roles = Object.values(foundUser.roles);
+//         // create JWTs
+//         const accessToken = jwt.sign({
+//             "UserInfo": {
+//                 "username": foundUser.username,
+//                 "roles": roles
+//             }
+//         },
+//             process.env.ACCESS_TOKEN_SECRET,
+//             { expiresIn: '30m' });
+
+//         const refreshToken = jwt.sign({
+//             "username": foundUser.username
+//         },
+//             process.env.REFRESH_TOKEN_SECRET,
+//             { expiresIn: '8h' });
+
+//         // saving refresh token with current user
+//         // try {
+//         //     const result = await User.updateOne(
+//         //         { username: foundUser.username },
+//         //         { $set: { refreshToken } });
+//         // }
+//         // catch (err) {
+//         //     res.status(500).json({ 'message': err.message });
+//         // }
+//         foundUser.refreshToken = refreshToken;
+//         const result = await foundUser.save();
+
+//         // res.cookie('jwt', refreshToken, { httpOnly: true, sameSite: 'None', secure: true, maxAge: 24 * 60 * 60 * 1000 });
+//         res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
+//         res.json({ accessToken });
+//     } else {
+//         res.sendStatus(401);
+//     }
+// };
+
+const handleLogin = async (req, res) => {
+    console.log(req.body);
+    res.send('ok');
+};
+
+module.exports = { handleLogin };
