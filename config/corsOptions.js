2728ce0372919abd30e827b14b37da9a4df6cae4
Jurek <jerzykomorowski77@gmail.com>
2023-11-02 19:51:58 +0100

    added  credentials: true, to corsOptions

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -9,7 +9,19 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
+    credentials: true,
     optionsSuccessStatus: 200
 };
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         // Sprawdź, czy pochodzenie jest na liście zaufanych origin
+//         if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error('Not allowed by CORS'));
+//         }
+//     },
+//     optionsSuccessStatus: 200
+// };
 
 module.exports = corsOptions;
