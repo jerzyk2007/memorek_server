60f3be952fb2947d06f276c360a546804329fe33
Jurek <jerzykomorowski77@gmail.com>
2023-11-02 18:21:20 +0100

    .

--- middleware/credentials.js
+++ middleware/credentials.js
@@ -5,8 +5,8 @@ const credentials = (req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.header('Access-Control-Allow-Origin', origin);
-        res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        res.header('Access-Control-Allow-Credentials', true);
     }
     if (req.method === 'OPTIONS') {
         res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
