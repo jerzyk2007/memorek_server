7781c2bdfcf6a9e213c05e792cfe686b5989ecdf
Jurek <jerzykomorowski77@gmail.com>
2023-11-02 18:57:49 +0100

    app.set('trust proxy', 1); added

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ const connectDB = require('./config/dbConn');
 const PORT = process.env.PORT || 3500;
 
 
+// cookie problem
+app.set('trust proxy', 1);
+
 // Handle options credentials check - before cors
 // and fetch cookies credentials requirement
 app.use(credentials);
