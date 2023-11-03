da62624aad18e5cf434ecd194884908c12efc40c
Jurek <jerzykomorowski77@gmail.com>
2023-11-03 10:34:11 +0100

    removed address

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,15 +1,11 @@
 const allowedOrigins = [
-    "https://www.google.pl",
     "http://127.0.0.1:5500",
     "http://localhost:4000",
     "http://localhost:3000",
     "http://127.0.0.1:3500",
     "http://localhost:3500",
     "https://front-web.pl",
-    "http://windykacja.auto.pl",
-    "https://salty-badlands-34718-49e2594dd237.herokuapp.com",
-    "*"
-
+    "http://windykacja.auto.pl"
 ];
 
 module.exports = allowedOrigins;
