e1fd0bff59d16503703a162ebeb51b65cfe44fbf
Jurek <jerzykomorowski77@gmail.com>
2023-11-03 17:51:44 +0100

    added sorted collections name

--- controllers/collectionsController.js
+++ controllers/collectionsController.js
@@ -4,8 +4,8 @@ const getCollectionsName = async (req, res) => {
     const collections = await mongoose.connection.db.listCollections().toArray();
     const collectionNames = collections.map((collection) => collection.name);
     const filteredCollectionNames = collectionNames.filter((name) => name !== "users");
-    res.json(filteredCollectionNames);
+    const sortedCollectionNames = filteredCollectionNames.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
+    res.json(sortedCollectionNames);
 };
 
-module.exports =
-    getCollectionsName;
+module.exports = getCollectionsName;
