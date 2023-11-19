1e4b21034c4b5211ae668cb7ab2a7b108fc725e6
Jurek <jerzykomorowski77@gmail.com>
2023-11-19 16:32:12 +0100

    cors

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -3,10 +3,10 @@ const allowedOrigins = require('../config/allowedOrigins');
 
 const corsOptions = {
     origin: (origin, callback) => {
-        if (allowedOrigins.indexOf(origin) === -1 || allowedOrigins.indexOf(origin) !== -1) {
+        // if (allowedOrigins.indexOf(origin) === -1 || allowedOrigins.indexOf(origin) !== -1) {
 
-            // if (allowedOrigins.indexOf(origin) !== -1) {
-            // if (allowedOrigins.indexOf(origin)) {
+        // if (allowedOrigins.indexOf(origin) !== -1) {
+        if (allowedOrigins.indexOf(origin) || !origin) {
             callback(null, true);
         } else {
             callback(new Error('Not allowed by CORS'));
